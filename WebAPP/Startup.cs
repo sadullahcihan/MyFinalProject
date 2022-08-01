bd5bd67f9f69b846e69f3e5f97dac04e00665f77
sadullahcihan <sado_sado1999@hotmail.com>
2022-08-01 10:11:19 +0300

    Dealing With API 01/08/2022 10:10

---
+++ WebAPP/Startup.cs
@@ -0,0 +1,66 @@
+using Business.Concrete;
+using Business.Abstract;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace WebAPP
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Autofac, Ninject, CastleWindsor, StructureMap, LigthInject, DryInject --> IoC Container
+            //AOP
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPP", Version = "v1" });
+            });
+            services.AddSingleton<IProductService,ProductManager>(); //sends reference to controller(no data)
+            services.AddSingleton<IProductDal, EfProductDal>(); //dependency resolution
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPP v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
